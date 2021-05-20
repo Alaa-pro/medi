a40d69196e1c12f8046a14c400d91d8ba5264c02
Alaa-pro <82983067+Alaa-pro@users.noreply.github.com>
2021-05-20 02:54:17 -1100

    Update medi.dart

--- lib/medi.dart
+++ lib/medi.dart
@@ -3,10 +3,156 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+
+
 class Medi extends StatelessWidget{
   @override
   // ignore: missing_return
   Widget build(BuildContext context) {
+    final List<String> e = <String>['Asprin', 'Bamadol', 'Colorent'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+    _showfromDialog(BuildContext context){
+      return showDialog(context: context, builder: (parm){
+        return AlertDialog(
+          title: Text("Add medi"),
+          content: SingleChildScrollView(
+            child: Container(
+              width: 700,
+            height: 400,
+            child: Column(
+              children: [
+                TextField(
+                  decoration:InputDecoration(
+                    labelText: "Name Medical",
+                    hintText: "Text",
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children:[
+                    Text("La clairence Renal"),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(child:TextField(
+
+                      decoration:InputDecoration(
+                        labelText: "Min",
+                      ),
+                      keyboardType: TextInputType.number,
+                    )),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(child: TextField(
+                      decoration:InputDecoration(
+                        labelText: "Max",
+                      ),
+                      keyboardType: TextInputType.number,
+                    )),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(child: TextField(
+                      decoration:InputDecoration(
+                        labelText: "Reuslt",
+                      ),
+                      keyboardType: TextInputType.number,
+                    )),
+                    SizedBox(
+                      height: 10,
+                    ),
+
+
+                        ]),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                    children:[
+                      Text("la bilirubine"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Flexible(child:TextField(
+                        decoration:InputDecoration(
+                          labelText: "Min",
+                        ),
+                        keyboardType: TextInputType.number,
+                      )),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Flexible(child: TextField(
+                        decoration:InputDecoration(
+                          labelText: "Max",
+                        ),
+                        keyboardType: TextInputType.number,
+                      )),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Flexible(child: TextField(
+                        decoration:InputDecoration(
+                          labelText: "Result",
+                        ),
+                        keyboardType: TextInputType.number,
+                      )),
+                      SizedBox(
+                        height: 10,
+                      ),
+
+
+                    ]),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                    children:[
+                      Text("tgo/tgp"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Flexible(child:TextField(
+                        decoration:InputDecoration(
+                          labelText: "Min",
+                        ),
+                        keyboardType: TextInputType.number,
+                      )),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Flexible(child: TextField(
+                        decoration:InputDecoration(
+                          labelText: "Max",
+                        ),
+                        keyboardType: TextInputType.number,
+                      )),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Flexible(child: TextField(
+                        decoration:InputDecoration(
+                          labelText: "Result",
+                        ),
+                        keyboardType: TextInputType.number,
+                      )),
+                      SizedBox(
+                        height: 10,
+                      ),
+
+
+                    ]),
+              ],
+            ),
+          ),
+          )
+
+        );
+      });
+    }
     // TODO: implement build
   return Scaffold(
     extendBodyBehindAppBar: true,
@@ -19,67 +165,71 @@ class Medi extends StatelessWidget{
 
     body: Container(
       decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("images/kk.png"),
-              fit: BoxFit.fill,
+
+        image: DecorationImage(
+              image: AssetImage("images/ss.png"),
+              fit: BoxFit.cover,
             ),
+
           ),
+
           child: Center(
-           child: ListView(
-            padding: const EdgeInsets.only(right: 20,left: 20,top: 80),
-            children: <Widget>[
-              Container(
-                height: 50,
-                color: Colors.amber[600],
-                child: const Center(child: Text('A')),
-              ),SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 50,
-                color: Colors.amber[500],
-                child: const Center(child: Text('B')),
-              ),SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 50,
-                color: Colors.amber[100],
-                child: const Center(child: Text('C')),
-              ),SizedBox(
-                height: 10,
-              ),
-            ],
-          )
+           child: ListView.separated(
+             padding: const EdgeInsets.only(left: 30,right: 30,top: 150),
+             itemCount: e.length,
+             itemBuilder: (BuildContext context, int index) {
+               return Container(
+                 height: 50,
+                 color: Colors.amber[colorCodes[index]],
+                 // ignore: deprecated_member_use
+                 child: RaisedButton(
+                   onPressed: () {},
+                   child: Text(
+                     ' ${e[index]}',
+
+                   ),
+                    color: Color(0xffFBD99E),//[colorCodes[index]],
+                 ),
+               );
+             },
+             separatorBuilder: (BuildContext context, int index) => const Divider(),
+           )
 
       ),
     ),
-    // floatingActionButton: FloatingActionButton(
-    //   child: Icon(Icons.add),
-    //   onPressed: () {},
-    //
-    // ),
-    // bottomNavigationBar: BottomAppBar(
-    //   shape: CircularNotchedRectangle(),
-    //   notchMargin: 8.0,
-    //   color: Colors.blue,
-    //   child: Container(
-    //     height: 50,
-    //     child: Row(
-    //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-    //       children: [
-    //         IconButton(icon: Icon(Icons.search,color: Colors.white,), onPressed: null),
-    //         IconButton(icon: Icon(Icons.delete,color: Colors.white,), onPressed: null),
-    //
-    //
-    //       ],
-    //     ),
-    //
-    //   ),
-    // ),
-    //
-    // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    floatingActionButton: FloatingActionButton(
+      backgroundColor: Color(0xff3AD1B7),
+      child: Icon(Icons.add,),
+      onPressed: () {
+        _showfromDialog(context);
+      },
+
+    ),
+    bottomNavigationBar: BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      notchMargin: 8.0,
+      color: Color(0xff3AD1B7),
+
+
+      child: Container(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(icon: Icon(Icons.search,color: Colors.white,), onPressed: null),
+            IconButton(icon: Icon(Icons.delete,color: Colors.white,), onPressed: null),
+
+
+          ],
+        ),
+
+      ),
+    ),
+
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
   );
   }
 }
+
+
