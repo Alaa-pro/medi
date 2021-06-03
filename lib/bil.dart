70ae1961776abe9f9f1c6cd4b2310267e2fc5c01
Alaa-pro <82983067+Alaa-pro@users.noreply.github.com>
2021-06-03 03:12:11 -1100

    Add files via upload

---
+++ lib/bil.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:medical/modules/note.dart';
+import 'package:medical/screen/ggg.dart';
+
+// ignore: must_be_immutable
+class Bil extends StatelessWidget{
+  Note note;
+  NoteListState ggg;
+  TextEditingController cont1=TextEditingController();
+  TextEditingController cont2=TextEditingController();
+  TextEditingController cont3=TextEditingController();
+  List<Note> noteList;
+  int count = 0;
+
+  @override
+  // ignore: missing_return
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+
+        title: Text(("bil")),
+        backgroundColor:Color(0xff3AD1B7),
+      ),
+      body: Container(
+          padding: const EdgeInsets.only(left: 30,right: 30,top: 120),
+              // decoration: BoxDecoration(
+              // image: DecorationImage(
+              // image: AssetImage("images/qq.png"),
+              // fit: BoxFit.cover,
+              // ),
+              // ),
+       //
+
+
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                      children:[
+                          Center(child: Text("La clairence Renal",style: TextStyle(fontSize: 20),)),
+                                Row(
+                                    children:[
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      Flexible(child:TextField(
+                                        controller: cont1,
+                                        // style: textStyle,
+                                        onChanged: (value) {
+                                          // debugPrint('Something changed in Title Text Field');
+                                          // updateMin();
+                                        },
+                                        decoration:InputDecoration(
+                                          labelText: "1",
+                                        ),
+                                        keyboardType: TextInputType.number,
+                                      )),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+
+
+
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                    ]),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                Center(child: Text("La clairence Renal",style: TextStyle(fontSize: 20),)),
+                                Row(
+                                    children:[
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      Flexible(child:TextField(
+                                        controller: cont2,
+                                        // style: textStyle,
+                                        onChanged: (value) {
+                                          // debugPrint('Something changed in Title Text Field');
+                                          // updateMin();
+                                        },
+                                        decoration:InputDecoration(
+                                          labelText: "2",
+                                        ),
+                                        keyboardType: TextInputType.number,
+                                      )),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                    ]),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                Center(child: Text("La clairence Renal",style: TextStyle(fontSize: 20),)),
+                                Row(
+                                    children:[
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      Flexible(child:TextField(
+                                        controller: cont3,
+                                        // style: textStyle,
+                                        onChanged: (value) {
+                                          // debugPrint('Something changed in Title Text Field');
+                                          // updateMin();
+                                        },
+                                        decoration:InputDecoration(
+                                          labelText: "3",
+                                        ),
+                                        keyboardType: TextInputType.number,
+                                      )),
+                                    ]),
+                        // ignore: deprecated_member_use
+                        RaisedButton(
+                          padding:EdgeInsets.only(left: 15,right: 15,top: 5,bottom: 5),
+                          color: Color(0xff3AD1B7),
+                          textColor: Colors.white,
+                          child: Text(
+                            'Select',
+                            textScaleFactor: 1.5,
+                            style: TextStyle(fontSize: 23),
+                          ),
+                          onPressed: () { Navigator.of(context).push(MaterialPageRoute(
+                              builder: (_) {
+                                return GGg();
+                              }
+                          ));
+                          },
+                          splashColor: Colors.deepOrange,
+
+                        ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          // ignore: deprecated_member_use
+                          RaisedButton(
+                            padding:EdgeInsets.only(left: 15,right: 15,top: 5,bottom: 5),
+                            color: Color(0xff3AD1B7),
+                            textColor: Colors.white,
+                            child: Text(
+                              'Result',
+                              textScaleFactor: 1.5,
+                              style: TextStyle(fontSize: 23),
+                            ),
+                            onPressed: () {
+
+                                debugPrint("Result button clicked");
+                                // _Calcule();
+                                int s1 = int.parse(cont1.text);
+                                // int s2 = int.parse(cont2.text);
+                                // int s3 = int.parse(cont3.text);
+                                ggg.calc(s1);
+                                print('$s1');
+
+
+                            },
+                            splashColor: Colors.deepOrange,
+                          ),
+
+                      ]
+                    )
+          ),
+            ),
+
+    )
+    );
+  }
+  void _Calcule() async {
+
+    int s1 = int.parse(cont1.text);
+    int s2 = int.parse(cont2.text);
+    int s3 = int.parse(cont3.text);
+
+    if (s1>20) {
+      print('done!');
+    }
+    if (s2>20) {
+      print('done!');
+    }
+    if (s3>20) {
+      print('done!');
+    }
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:medical/bil.dart';
+import 'package:medical/screen/medi.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: Text(("App")),
+          centerTitle: true,
+          backgroundColor: Color(0xff3AD1B7),
+          elevation: 0,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/aa.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Stack(children: [
+            Positioned(
+                left: 76, top: 553,
+                width: 70,
+                height: 70,
+
+                // ignore: deprecated_member_use
+                child: IconButton(
+                  icon:Icon(Icons.arrow_drop_down, size: 50.0,color: Color(0xff3AD1B7,)),
+                  // icon: Icon(Icons.search,color: Color(0xff3AD1B7, )),
+                 // splashColor: Colors.black26,
+                  onPressed: () { Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) {
+                        return NoteList();
+                      }
+                  ));
+                  },
+
+
+            )
+            ),
+            Positioned(
+                right: 70, top: 545,
+                width: 62,
+                height: 62,
+                child: IconButton(
+                  icon:Icon(Icons.arrow_right, size: 50.0,color: Color(0xff3AD1B7,)),
+                 // splashColor: Colors.black26,
+                  onPressed: () { Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) {
+                        return Bil();
+                      }
+                  ));
+                  },
+
+
+
+                )),
+            
+
+            
+
+          ]),
+        ));
+  }
+}
